6ae59b0a655323232fe126a19f878bd1bd1c3496
tiedyeguy <jeis1901@mpsedu.org>
2020-08-27 15:08:07 -0500

    setState for cell, untested drawing, frame around grid

--- src/Driver.java
+++ src/Driver.java
@@ -21,26 +21,52 @@ public class Driver extends PApplet {
 	}
 
 	public void setup() {
-		Settings.init(true, Dimension.TWO_TIME, NeighborType.NEUMANN);
-		Cell.setSize(new PVector(20, 30, 40));
+		// TODO: replace grid dimensions and settings initialization with what's given
+		// by the json; update cell size accordingly
+		grid = new Grid(2, 3, 4);
+		Settings.init(true, Dimension.TWO, NeighborType.NEUMANN);
+		Cell.setSize(new PVector(400, 800 / 3f, 1));
+		// penState should initially be set to the first state (not the default state)
+		// set up ruleset
 
-		grid = new Grid(3, 3, 3);
+		// TODO: fill in initial states according to json
+
 		camera = new PeasyCam(this, 200);
-		userDrawing = true;
-
 		camera.setActive(Settings.getDimension() == Dimension.TWO_TIME || Settings.getDimension() == Dimension.THREE);
-
-		stroke(255);
+		userDrawing = !camera.isActive();
 	}
 
 	public void draw() {
-		background(100);
+		background(0);
 
+		if (userDrawing) {
+			noCursor();
+			fill(penState.getRed(), penState.getGreen(), penState.getGreen());
+			circle(mouseX, mouseY, 20);
+		} else {
+			cursor();
+		}
+
+		grid.prepareAllStates();
+		grid.updateAllStates();
 		grid.draw(this);
 	}
 
-	public void mousePressed() {
-//		if(userDrawing)
+	public void mouseClicked() {
+		mouseDown();
+	}
+
+	public void mouseDragged() {
+		mouseDown();
+	}
+
+	public void mouseDown() {
+		if (userDrawing) {
+			if(mouseButton == LEFT)
+			grid.handleClick(mouseX, mouseY, penState);
+			else if(mouseButton == RIGHT)
+				grid.handleClick(mouseX, mouseY, State.getState("default"));
+		}
 	}
 
 	public void keyPressed() {
